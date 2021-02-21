1f13c008e6cb84a49cf3842579e0e91db81775be
Ajay Sethi <ajay.sethi@datdyn.com>
2021-02-22 00:20:09 +0530

    commented out the node.kt

--- ticket-nodes/src/main/kotlin/com/r3/demo/nodes/Node.kt
+++ ticket-nodes/src/main/kotlin/com/r3/demo/nodes/Node.kt
@@ -1,32 +1,32 @@
-package com.r3.demo.nodes
-
-import net.corda.core.identity.Party
-
-/**
- * Records the node information for a user.
- */
-class Node(val main: Main,
-           val name: String,
-           val legalName: String,
-           val username: String,
-           val password: String,
-           val address: String){
-
-    var party: Party? = null
-
-    override fun toString(): String {
-        return legalName
-    }
-
-    fun isIssuer(): Boolean {
-        return legalName.contains("bank", true)
-    }
-
-    fun isCertifier(): Boolean {
-        return legalName.contains("certifier", true)
-    }
-
-    fun isDealer(): Boolean {
-        return legalName.contains("dealer", true)
-    }
-}
+//package com.r3.demo.nodes
+//
+//import net.corda.core.identity.Party
+//
+///**
+// * Records the node information for a user.
+// */
+//class Node(val main: Main,
+//           val name: String,
+//           val legalName: String,
+//           val username: String,
+//           val password: String,
+//           val address: String){
+//
+//    var party: Party? = null
+//
+//    override fun toString(): String {
+//        return legalName
+//    }
+//
+//    fun isIssuer(): Boolean {
+//        return legalName.contains("bank", true)
+//    }
+//
+//    fun isCertifier(): Boolean {
+//        return legalName.contains("certifier", true)
+//    }
+//
+//    fun isDealer(): Boolean {
+//        return legalName.contains("dealer", true)
+//    }
+//}
